b14db1bc4f203c26db6f8882071a4bcc5bbf63cf
Honghua Zhu <zhh200910@gmail.com>
2023-11-09 08:16:45 +0800

    已经在ProtocolServerEngineBase中统一负责各种api的实现

--- src/main/java/org/lealone/plugins/oracle/server/OracleServerEngine.java
+++ src/main/java/org/lealone/plugins/oracle/server/OracleServerEngine.java
@@ -5,32 +5,19 @@
  */
 package org.lealone.plugins.oracle.server;
 
-import java.util.Map;
-
 import org.lealone.server.ProtocolServer;
 import org.lealone.server.ProtocolServerEngineBase;
 
 public class OracleServerEngine extends ProtocolServerEngineBase {
 
     public static final String NAME = "Oracle";
-    private final OracleServer server = new OracleServer();
 
     public OracleServerEngine() {
         super(NAME);
     }
 
     @Override
-    public ProtocolServer getProtocolServer() {
-        return server;
-    }
-
-    @Override
-    public void init(Map<String, String> config) {
-        server.init(config);
-    }
-
-    @Override
-    public void close() {
-        server.stop();
+    protected ProtocolServer createProtocolServer() {
+        return new OracleServer();
     }
 }
